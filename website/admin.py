7e3e6bd64fdb7033a3ebeba4d6419a11cf28ed67
farnam mansouri <farnammansouri@yahoo.com>
2019-01-01 15:42:27 +0330

    Farnam: Models updated

--- website/admin.py
+++ website/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 
-from .models import Employer, Phone, WorkSeeker, User
+from .models import *
 
 admin.site.register(User)
 admin.site.register(Employer)
 admin.site.register(WorkSeeker)
 admin.site.register(Phone)
+admin.site.register(CV)
+admin.site.register(SystemAdmin)
+admin.site.register(JobOffer)
+admin.site.register(JobRequest)
+admin.site.register(Requirement)
--- website/models.py
+++ website/models.py
@@ -19,8 +19,11 @@ class WorkSeeker(models.Model):
     studentID = models.CharField(max_length=8, primary_key=True,default="95109253")
     nationalID = models.CharField(max_length=10,default="1234567891")
     address = models.CharField(max_length=400,default="dummy")
-    sex = models.BooleanField(default=0)
-    birth_date = models.DateTimeField('date published')
+
+    sex_Choices = [("1", "MALE"), ("2","FEMALE")]
+    sex = models.CharField(max_length=9, choices=sex_Choices, default="FEMALE")
+
+    birth_date = models.DateField()
 
     def __str__(self):
         return self.name + " " + self.family_name
@@ -34,6 +37,12 @@ class Phone(models.Model):
     phoneNumber = models.CharField(validators=[phone_regex], max_length=17, blank=True,default="+98912988888")
 
 
+class CV(models.Model):
+    workSeeker = models.ForeignKey(WorkSeeker, on_delete=models.CASCADE)
+    CV_text = models.FileField()
+    picture = models.FileField()
+
+
 class Employer(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
@@ -41,3 +50,37 @@ class Employer(models.Model):
     companyAddress = models.CharField(max_length=400,default="dummy")
     companyWebsite = models.URLField(default="google.com")
     rate = models.FloatField(default=1.1)
+
+
+class JobOffer(models.Model):
+    employer =  models.ForeignKey(Employer, on_delete=models.CASCADE)
+
+    description = models.CharField(max_length=200,default="dummy")
+
+    status_Choices = [("1","AVAILABLE"), ("2","NOT AVAILABLE")]
+    status = models.CharField(max_length=20, choices=status_Choices, default="AVAILABLE")
+
+    salary = models.IntegerField(default= 1000)
+
+
+class Requirement(models.Model):
+    jobOffer = models.ForeignKey(JobOffer, on_delete=models.CASCADE)
+
+    text = models.CharField(max_length=200,default="dummy")
+
+
+class JobRequest(models.Model):
+    employer = models.ForeignKey(Employer, on_delete=models.CASCADE)
+    workSeeker = models.ForeignKey(WorkSeeker, on_delete=models.CASCADE)
+
+    date = models.DateField()
+
+    status_Choices = [("1", "AVAILABLE"), ("2", "NOT AVAILABLE")]
+    status = models.CharField(max_length=20, choices=status_Choices, default="AVAILABLE")
+
+
+class SystemAdmin(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+
