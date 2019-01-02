bac360cb92c16cd75dceeaed386b313ced236b2c
Amin <amin.bahjati@gmail.com>
2019-01-02 12:31:04 +0330

    refactor code

--- website/admin.py
+++ website/admin.py
@@ -4,10 +4,10 @@ from .models import *
 
 admin.site.register(User)
 admin.site.register(Employer)
-admin.site.register(WorkSeeker)
+admin.site.register(Employee)
 admin.site.register(Phone)
 admin.site.register(CV)
 admin.site.register(SystemAdmin)
-admin.site.register(JobOffer)
+admin.site.register(EmpOff)
 admin.site.register(JobRequest)
 admin.site.register(Requirement)
