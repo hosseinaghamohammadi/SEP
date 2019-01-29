0abb0d16be87c835ed1d4c05c146bf58efb2d876
farnam mansouri <farnammansouri@yahoo.com>
2019-01-30 00:38:21 +0330

    backend sign up sing in

--- website/urls.py
+++ website/urls.py
@@ -11,5 +11,12 @@ urlpatterns = [
     path('employee/', views.employee, name='employee profile'),
     path('employer/', views.employer, name='employer profile'),
     path('edit_profile/', views.edit_profile, name='edit_profile'),
-    path('employee_home/', views.employee_home, name='employee homepage')
+    path('employee_home/', views.employee_home, name='employee homepage'),
+    path('getMail/<int:type>/', views.get_mail, name='get mail'),
+    path('verifyMail/<int:type>/', views.verify_mail, name='verify mail'),
+    path('fillFormEmployee/<str:mail>', views.fill_form_employee, name='fill form employee'),
+    path('fillFormEmployer/<str:mail>', views.fill_form_employer, name='fill form employer'),
+    path('verifyFormEmployee/<str:mail>/', views.verify_form_employee, name='verify form employee'),
+    path('verifyFormEmployer/<str:mail>/', views.verify_form_employer, name='verify form employer'),
+    path('SignUpEmployee/', views.sign_up_employee, name='Sign Up Employee'),
 ]
